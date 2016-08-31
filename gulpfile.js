55573393637b2138ff935e19ada7c70db0faf253
Phillip Raffnsøe <praffn@Phillips-MacBook-Pro.local>
2016-08-31 20:17:35 +0200

    modernize build process

--- gulpfile.js
+++ gulpfile.js
@@ -1,48 +1,48 @@
-var gulp = require('gulp'),
-		sass = require('gulp-sass'),
-		plumber = require('gulp-plumber'),
-		autoprefixer = require('gulp-autoprefixer'),
-		imagemin = require('gulp-imagemin'),
-		uglify = require('gulp-uglify'),
-		rename = require('gulp-rename'),
-		minifyCSS = require('gulp-minify-css');
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const plumber = require('gulp-plumber');
+const autoprefixer = require('gulp-autoprefixer');
+const imagemin = require('gulp-imagemin');
+const uglify = require('gulp-uglify');
+const rename = require('gulp-rename');
+const minifyCSS = require('gulp-minify-css');
 
 gulp.task('default', ['sass', 'watch', 'js', 'imagemin']);
 
 // sass
-gulp.task('sass', function() {
-	gulp.src('./src/sass/main.scss')
-		.pipe(plumber())
-		.pipe(sass())
-		.pipe(autoprefixer())
-		.pipe(gulp.dest('./dist/assets/css/'))
-		.pipe(rename({ suffix: '.min' }))
-		.pipe(minifyCSS())
-		.pipe(gulp.dest('./dist/assets/css'));
+gulp.task('sass', () => {
+  gulp.src('./src/sass/main.scss')
+    .pipe(plumber())
+    .pipe(sass())
+    .pipe(autoprefixer())
+    .pipe(gulp.dest('./dist/assets/css/'))
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(minifyCSS())
+    .pipe(gulp.dest('./dist/assets/css'));
 });
 
 // img optimization
-gulp.task('imagemin', function() {
-	gulp.src('./src/images/**/*')
-		.pipe(plumber())
-		.pipe(imagemin())
-		.pipe(gulp.dest('./dist/assets/images'));
+gulp.task('imagemin', () => {
+  gulp.src('./src/images/**/*')
+    .pipe(plumber())
+    .pipe(imagemin())
+    .pipe(gulp.dest('./dist/assets/images'));
 });
 
 // javascript
-gulp.task('js', function() {
-	gulp.src('./src/js/**/*.js')
-		.pipe(plumber())
-		.pipe(gulp.dest('./dist/assets/js'))
-		.pipe(rename({ suffix: '.min' }))
-		.pipe(uglify())
-		.pipe(gulp.dest('./dist/assets/js'));
+gulp.task('js', () => {
+  gulp.src('./src/js/**/*.js')
+    .pipe(plumber())
+    .pipe(gulp.dest('./dist/assets/js'))
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(uglify())
+    .pipe(gulp.dest('./dist/assets/js'));
 });
 
 gulp.task('once', ['sass', 'js', 'imagemin']);
 
-gulp.task('watch', function() {
-	gulp.watch('./src/sass/**/*.scss', ['sass']);
-	gulp.watch('./src/images/*', ['imagemin']);
-	gulp.watch('./src/js/**/*.js', ['js']);
+gulp.task('watch', () => {
+  gulp.watch('./src/sass/**/*.scss', ['sass']);
+  gulp.watch('./src/images/*', ['imagemin']);
+  gulp.watch('./src/js/**/*.js', ['js']);
 });
--- serve.js
+++
@@ -1,4 +0,0 @@
-var e = require('express'),
-		a = e();
-
-a.use(e.static(__dirname+'/dist/')).listen(3000);
